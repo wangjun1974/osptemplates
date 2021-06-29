6b8209b296f3fc6f55d5482ad42441245f9b8d65
Wang Jun <wangjun1974@gmail.com>
2021-06-29 14:37:08 +0800

    add ovsdpdksriov templates

---
+++ osp16.1/ovs-dpdk/plan-deploy.sh
@@ -0,0 +1,23 @@
+CUSTOM=/home/stack/ovs-dpdk
+
+openstack overcloud deploy \
+ --templates --update-plan-only \
+ -r /home/stack/ovs-dpdk/templates/roles_data.yaml \
+ -n /home/stack/ovs-dpdk/templates/network_data.yaml \
+ -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+ -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-dashboard.yaml \
+ -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-rgw.yaml \
+ -e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \
+ -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+ -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
+ -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
+ -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+ -e $CUSTOM/containers-prepare-parameter.yaml \
+ -e $CUSTOM/templates/ceph_config.yaml \
+ -e $CUSTOM/templates/node_info.yaml \
+ -e $CUSTOM/templates/network-environment.yaml \
+ -e $CUSTOM/templates/overcloud-image.yaml \
+ -e $CUSTOM/templates/root_password.yaml \
+ -e $CUSTOM/templates/externalvip.yaml \
+ -e $CUSTOM/templates/fencing.yaml \
+ -p $CUSTOM/plan-environment-derived-params.yaml \
